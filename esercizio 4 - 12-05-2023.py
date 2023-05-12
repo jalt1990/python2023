a3170453d0c4e535f52c4268adfd13e20c74ae69
jalt1990 <francorinaldi90@gmail.com>
2023-05-12 12:50:18 +0200

    Esercizio 4

---
+++ esercizio 4 - 12-05-2023.py
@@ -0,0 +1,20 @@
+'''Scrivere un programma che prenda una lista di numeri come input 
+   e rimuove i duplicati dalla lista, lasciando solo i valori "unici".
+   L'ordine degli elementi nella lista deve rimanere invariato'''
+
+# inizializzazione della lista numeri
+numeri = input("scrivi i numeri separati da una virgola:").replace(' ','').split(',')
+
+# inizializzazione della lista di output
+output = []
+
+# esplorazione della lista numeri e aggiunge in coda soltanto se non presente
+for numero in numeri:
+    if numero not in output:
+        output.append(numero)
+
+# output
+print('la lista inserita è:\n', numeri)
+print('togliendo occorrenze ridondandi diventa:\n', output)
+
+# stringa da usare come esempio : 1,2,33,15,1,2,15,33,4
