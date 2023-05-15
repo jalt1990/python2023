d1faa2d25301fb0e45e646c23897fb3e6d1ca638
jalt1990 <francorinaldi90@gmail.com>
2023-05-15 12:50:42 +0200

    esercizi su classi e oggetti

---
+++ Settimana Mirko/giorno_1.py
@@ -0,0 +1,46 @@
+# GIORNO 1:
+# LEZIONE DI CLASSI E OGGETTI
+
+# viene creata una classe Tazza
+class Tazza():
+    # attributo 'contenuto' di tipo stringa
+    contenuto = 'Buzz-Cola'
+
+class Bottiglia():
+    # attributo 'capacitaInLitri' di tipo int // l'attributo è anche detto "variabile della classe"
+    capacitaInLitri = 3
+
+# viene creato un oggetto di tipo tazza
+# viene creata una variabile e le viene associata il tipo Tazza
+oggetto_1 = Tazza()
+oggetto_2 = Tazza()
+oggetto_3 = Tazza()
+
+# modifica del contenuto dell'oggetto_1 (POLIMORFISMO):
+oggetto_1.contenuto = 'Caffe'
+# non avendo metodi "tipizzati", l'attributo può essere di qualsiasi tipo 
+oggetto_2.contenuto = 10    # anche numerico
+
+print(oggetto_1.contenuto)
+print(oggetto_2.contenuto)
+print(oggetto_3.contenuto)
+
+# creazione della classe contenente molti tipi
+class MultiType():
+    numeroInt = 0
+    numeroFloat = 0.0
+    stringa = 'sono una stringa'
+    booleano = False
+
+# cambiare il contenuto degli attributi
+oggettoMultiType = MultiType()
+oggettoMultiType.numeroInt = 33
+oggettoMultiType.numeroFloat = 5.55
+oggettoMultiType.stringa = 'Ciao sono una stringa'
+oggettoMultiType.booleano = True
+
+# mandare in stampa gli attributi
+print(oggettoMultiType.numeroInt)
+print(oggettoMultiType.numeroFloat)
+print(oggettoMultiType.stringa)
+print(oggettoMultiType.booleano)
